5243225310e409d32dc08501f931d05f0a6e4008
sgalal <sgalal.me@outlook.com>
2020-12-21 09:58:22 +0800

    Update page
    1. 修改打字錯誤
    2. 重新引入 HanaMin web font，令移動電話亦能顯示生僻文字
    3. 增大單字間距，便於選字複製
    4. 「無字」提示改為以 CSS 實作
    5. 改善程式碼

--- index.js
+++ index.js
@@ -3,18 +3,26 @@
 function handleSubmit() {
 	try {
 		const text = document.getElementById('inputArea').value.trim();
-		const res = [];
+		const fragment = document.createDocumentFragment();
 		for (let i = 1; i <= 3874; i++) {
 			if (Qieyun.get音韻地位(i).屬於(text)) {
 				for (const [字頭, 解釋] of Qieyun.query小韻號(i)) {
-					res.push(`<a class="char" target="_blank" href="https://ytenx.org/zim?dzih=${encodeURIComponent(字頭)}&dzyen=1">${字頭}</a>`);
+					const a = document.createElement('a');
+					a.classList.add('char');
+					a.target = '_blank';
+					a.href = `https://ytenx.org/zim?dzih=${encodeURIComponent(字頭)}&dzyen=1`;
+					a.innerText = 字頭;
+					fragment.appendChild(a);
 				}
 			}
 		}
-		document.getElementById('outputArea').innerHTML = res.length === 0 ? '〈無字〉' : res.join('');
+		document.getElementById('outputArea').innerHTML = '';
+		document.getElementById('outputArea').appendChild(fragment);
 		document.getElementById('errorArea').innerText = '';
 	} catch (error) {
 		document.getElementById('outputArea').innerHTML = '';
 		document.getElementById('errorArea').innerText = `${error}\n${error.stack}`;
 	}
 }
+
+window.addEventListener('DOMContentLoaded', (event) => handleSubmit());
