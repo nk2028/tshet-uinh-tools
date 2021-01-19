2616dbe0fbd827d705f6f6ab5ecd5519a2780c91
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-01-19 09:56:58 +0800

    Add a switch to hide rare characters

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 'use strict';
 
+let commonChars;
+
+fetch('https://raw.githubusercontent.com/ayaka14732/syyon-vencie/69bc015b5c3df4cb3f1a856d49d9bf8a3271396e/texts/%E5%B8%B8%E7%94%A8%E5%AD%97%E9%A0%BB%E5%BA%8F%E8%A1%A8.txt')
+.then((response) => response.text())
+.then((text) => commonChars = new Set(text));
+
 function handleSubmit() {
+	const showRareChar = document.getElementById('rareCharSwitch').checked;
+
+	if (!showRareChar && commonChars == null) {
+		alert('Loading, please wait...');
+		return;
+	}
+
 	try {
 		const text = document.getElementById('inputArea').value.trim();
 		const fragment = document.createDocumentFragment();
 		for (const 音韻地位 of Qieyun.iter音韻地位()) {
 			if (音韻地位.屬於(text)) {
 				for (const { 字頭 } of 音韻地位.條目) {
-					const a = document.createElement('a');
-					a.classList.add('char');
-					a.target = '_blank';
-					a.href = `https://ytenx.org/zim?dzih=${encodeURIComponent(字頭)}&dzyen=1`;
-					a.innerText = 字頭;
-					fragment.appendChild(a);
+					if (showRareChar || commonChars.has(字頭)) {
+						const a = document.createElement('a');
+						a.classList.add('char');
+						a.target = '_blank';
+						a.href = `https://ytenx.org/zim?dzih=${encodeURIComponent(字頭)}&dzyen=1`;
+						a.innerText = 字頭;
+						fragment.appendChild(a);
+					}
 				}
 			}
 		}
@@ -24,5 +39,3 @@ function handleSubmit() {
 		document.getElementById('errorArea').innerText = `${error}\n${error.stack}`;
 	}
 }
-
-window.addEventListener('DOMContentLoaded', (event) => handleSubmit());
