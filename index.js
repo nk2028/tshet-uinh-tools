914940f5115a617842e09ff219cd8d4ca616e8a2
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-02-27 11:24:56 +0800

    Add functions

--- index.js
+++ index.js
@@ -15,23 +15,36 @@ function 創建單字HTML(字頭) {
 	return a;
 }
 
-function 根據音韻表達式查字(用户輸入, 顯示生僻字) {
-	if (!顯示生僻字 && 常見字 == null) {
+function 根據音韻表達式查字(用户輸入) {
+	const 顯示哪些字 = document.getElementById('form顯示哪些字').顯示哪些字.value;
+
+	if (顯示哪些字 === '只顯示常用字' && 常見字 == null) {
 		alert('Loading, please wait...');
 		return;
 	}
 
 	try {
-		const fragment = document.createDocumentFragment();
+		const res = new Set();
+
 		for (const 音韻地位 of Qieyun.iter音韻地位()) {
 			if (音韻地位.屬於(用户輸入)) {
-				for (const { 字頭 } of 音韻地位.條目) {
-					if (顯示生僻字 || 常見字.has(字頭)) {
-						fragment.appendChild(創建單字HTML(字頭));
+				if (顯示哪些字 === '一個音韻地位只顯示一個代表字') {
+					res.add(音韻地位.代表字);
+				} else {
+					for (const { 字頭 } of 音韻地位.條目) {
+						if (顯示哪些字 === '顯示所有字' || 常見字.has(字頭)) {
+							res.add(字頭);
+						}
 					}
 				}
 			}
 		}
+
+		const fragment = document.createDocumentFragment();
+		for (const 字頭 of res) {
+			fragment.appendChild(創建單字HTML(字頭));
+		}
+
 		document.getElementById('outputArea').innerHTML = '';
 		document.getElementById('outputArea').appendChild(fragment);
 		document.getElementById('errorArea').innerText = '';
@@ -41,7 +54,9 @@ function 根據音韻表達式查字(用户輸入, 顯示生僻字) {
 	}
 }
 
-function 根據音韻描述查字(用户輸入, 顯示生僻字) {
+function 根據音韻描述查字(用户輸入) {
+	const 顯示哪些字 = document.getElementById('form顯示哪些字').顯示哪些字.value;
+
 	let 音韻地位;
 	try {
 		音韻地位 = Qieyun.音韻地位.from描述(用户輸入);
@@ -50,12 +65,24 @@ function 根據音韻描述查字(用户輸入, 顯示生僻字) {
 		document.getElementById('errorArea').innerText = `${err}\n${err.stack}`;
 		return;
 	}
-	const fragment = document.createDocumentFragment();
-	for (const { 字頭 } of 音韻地位.條目) {
-		if (顯示生僻字 || 常見字.has(字頭)) {
-			fragment.appendChild(創建單字HTML(字頭));
+
+	const res = new Set();
+
+	if (顯示哪些字 === '一個音韻地位只顯示一個代表字' && 音韻地位.代表字 != null) {
+		res.add(音韻地位.代表字);
+	} else {
+		for (const { 字頭 } of 音韻地位.條目) {
+			if (顯示哪些字 === '顯示所有字' || 常見字.has(字頭)) {
+				res.add(字頭);
+			}
 		}
 	}
+
+	const fragment = document.createDocumentFragment();
+	for (const 字頭 of res) {
+		fragment.appendChild(創建單字HTML(字頭));
+	}
+
 	document.getElementById('outputArea').innerHTML = '';
 	document.getElementById('outputArea').appendChild(fragment);
 	document.getElementById('errorArea').innerText = '';
