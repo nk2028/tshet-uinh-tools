c502fe6f446a59293d0a4082657eb238b6f1a29c
Jwiet-Puj Drin | 姚妫颍 <Jwiet-Puj_Drin@linguistics.ac.cn>
2024-01-16 16:40:47 +0800

    Switch to Vite, optimize the method for fetching `常用字頻序表.txt`, introduce Google Fonts, and complete favicon and `robots.txt` | 更換爲 Vite ，優化獲取 `常用字頻序表.txt` 的方法，引入 Google Fonts，補全 favicon 和 `robots.txt`
    1. Node.js@16 ended its lifecycle on September 11, 2023. Consider updating it to the latest version 21.
    
    2. Consider using pnpm for module management.
    
    3. Parcel has been hesitating for a long time to update its dependency `svgo` (see [Parcel PR #8802](https://github.com/parcel-bundler/parcel/pull/8802 "feat: add @parcel/optimizer-svgo3 plugin by jack0pan · Pull Request #8802 · parcel-bundler/parcel")), leading to frequent warning prompts. Therefore, try switching the bundling tool to Vite.
    
    4. Try writing the script `postinstall.mjs` to replace the current method of using the system software Wget to fetch [常用字頻序表.txt](https://github.com/ayaka14732/syyon-vencie/blob/main/texts/常用字頻序表.txt "syyon-vencie/texts/常用字頻序表.txt at main · ayaka14732/syyon-vencie"), enhancing the maintainability of this project on operating systems without Wget installed.
    
    5. Consider introducing Google Fonts to improve display effects.
    
    6. Complete the favicon and a `robots.txt` example.
    
    Demo site of the committed version: [c89611c2.qieyun-tools-forked-by-jwietpuj-drin.pages.dev](https://c89611c2.qieyun-tools-forked-by-jwietpuj-drin.pages.dev/ "音韻地位查詢器")
    
    ---
    
    1. Node.js@16 已於 2023 年 9 月 11 日結束其生命週期，試更新其至最新版本 21 。
    
    2. 試使用 pnpm 管理模塊。
    
    3. Parcel 長時間在是否更新其依賴 `svgo` 的問題上踟躕不前（參見其倉庫下的 [PR #8802](https://github.com/parcel-bundler/parcel/pull/8802 "feat: add @parcel/optimizer-svgo3 plugin by jack0pan · Pull Request #8802 · parcel-bundler/parcel")），致使警告提示頻出，故嘗試更換打包工具爲 Vite 。
    
    4. 試編寫腳本 `postinstall.mjs` 以替換現採取調用系統軟件 Wget 獲取 [常用字頻序表.txt](https://github.com/ayaka14732/syyon-vencie/blob/main/texts/常用字頻序表.txt "syyon-vencie/texts/常用字頻序表.txt at main · ayaka14732/syyon-vencie") 的方法，增進本項目在未安裝 Wget 的操作系統下的可維護性。
    
    5. 試引入 Google Fonts 增進顯示效果。
    
    6. 補全 favicon 和一個 `robots.txt` 示例。
    
    提交版本的演示站點： [c89611c2.qieyun-tools-forked-by-jwietpuj-drin.pages.dev](https://c89611c2.qieyun-tools-forked-by-jwietpuj-drin.pages.dev/ "音韻地位查詢器")

---
+++ tools/postinstall.mjs
@@ -0,0 +1,21 @@
+/* https://nodejs.org/docs/latest/api/esm.html#https-and-http-imports
+ *
+ * Since node.JS@21.6.0 does not yet support `http(s)://` as a URL for module import, this script is needed to fetch the `常用字頻序表.txt`.
+ * 因之 node.JS@21.6.0 尚不能支援 `http(s)://` 爲模塊導入 URL ，故需要此脚本獲取 `常用字頻序表.txt` 。
+ * 
+ */
+
+import https from 'https';
+import fs from 'fs';
+const url = 'https://quantil.jsdelivr.net/gh/ayaka14732/syyon-vencie@main/texts/常用字頻序表.txt';
+
+https.get(url, (res) => {
+  const filePath = fs.createWriteStream('./src/常用字頻序表.txt');
+  res.pipe(filePath);
+  filePath.on('finish', () => {
+    filePath.close();
+    console.log('`常用字頻序表.txt` Updated too | `常用字頻序表.txt` 也已更新');
+  });
+}).on('error', (err) => {
+  console.log('Error | 錯誤: ', err.message);
+});
